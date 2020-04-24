9d21f87bc6e21f3b3589c922473a1ae1a85d4528
eastline <eastline@maycan.co.kr>
2020-04-24 15:30:12 +0900

    #6.5 MongoStore and Middlewares

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {
-  localsMiddleware
-} from "./middlewares";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globlaRouter from "./routers/globalRouter";
 
+import "./passport";
+
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -25,6 +31,17 @@ app.use(
   })
 );
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globlaRouter);
