ed3f44e5614ce45d6d1fb613d895649899f2c11a
eastline <eastline@maycan.co.kr>
2020-04-25 22:02:23 +0900

    #6.10.1 User Detail

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Cloning Youtube with Vanilla and NodeJS
 
 ## Pages:
 
-- [ ] Home
+- [x] Home
 - [x] Join
 - [x] Login
 - [x] Search
 - [ ] User Detail
 - [ ] Edit Profile
 - [ ] Change Password
-- [ ] Upload
-- [ ] Edit Video
+- [x] Upload
+- [x] Edit Video
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,24 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
-import { home, search } from "../controllers/videoController";
+import {
+  home,
+  search
+} from "../controllers/videoController";
 import {
   getJoin,
   postJoin,
   getLogin,
   postLogin,
   logout,
+  githubLogin,
+  postGithubLogin,
+  getMe
 } from "../controllers/userController";
-import { onlyPublic } from "../middlewares";
+import {
+  onlyPublic,
+  onlyPrivate
+} from "../middlewares";
 
 const globalRouter = express.Router();
 
@@ -20,6 +30,14 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(routes.githubCallback, passport.authenticate("github", {
+  failureRedirect: routes.login
+}), postGithubLogin);
+
+globalRouter.get(routes.me, getMe);
 
 export default globalRouter;
