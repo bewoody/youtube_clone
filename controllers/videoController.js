76d8dc15ee4870bc407c3ae007107dd77f6b31cb
eastline <eastline@maycan.co.kr>
2020-07-24 00:39:49 +0900

    #7.3 Adding Creator to Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -68,7 +68,10 @@ export const postUpload = async (req, res) => {
         fileUrl: path,
         title,
         description,
+        creator: req.user.id
     });
+    req.uesr.videos.push(newVideo.id);
+    req.user.save();
     res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -79,7 +82,7 @@ export const videoDetail = async (req, res) => {
         },
     } = req;
     try {
-        const video = await Video.findById(id);
+        const video = await (await Video.findById(id)).populated('creator');
         res.render("videoDetail", {
             pageTitle: video.title,
             video,
